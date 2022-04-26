c3f09efd1d00e79d6eb5767982ffd4d46ce6c9cb
doyu <ydh0905@nate.com>
2022-04-26 13:52:03 +0900

    creat: MainRelease 컴포넌트

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,6 +3,7 @@ import './Main.scss';
 import MainBannerWrapper from './MainBannerWrapper/MainBannerWrapper';
 import MainCarouselWrapper from './MainCarouselWrapper/MainCarouselWrapper';
 import MainHistoryWrapper from './MainHistoryWrapper/MainHistoryWrapper';
+import MainReleaseWrapper from './MainReleaseWrapper/MainReleaseWrapper';
 
 const Main = () => {
   return (
@@ -10,6 +11,9 @@ const Main = () => {
       <MainBannerWrapper />
       <MainCarouselWrapper />
       <MainHistoryWrapper />
+      <MainReleaseWrapper />
+      <header className="mainTransitionHeader">탁월한 셀렉션</header>
+      <MainCarouselWrapper />
     </>
   );
 };
---
+++ src/pages/Main/MainReleaseWrapper/MainReleaseWrapper.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './MainReleaseWrapper.scss';
+import MainContext from '../MainContext/MainContext';
+
+const MainReleaseWrapper = props => (
+  <section className="mainReleaseWrapper">
+    <div className="mainRelease">
+      <div className="mainReleaseImgBox">
+        <img
+          className="mainReleaseImg"
+          alt="releaseImg"
+          src="https://www.aesop.com/u1nb1km7t5q7/FFQtHLIDFtqckp6PILx1u/f3f47abb300aa878ed95ba182b71c744/Aesop_Rick_Owens_2022_Web_Homepage_Secondary_1_Mid_Desktop_2560x1440px.jpg"
+        />
+      </div>
+
+      <div className="mainReleaseContent">
+        <MainContext color="black" />
+      </div>
+    </div>
+  </section>
+);
+
+export default MainReleaseWrapper;
