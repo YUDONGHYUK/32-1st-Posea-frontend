40305007efed8607fb33407c840e0e65a3dd8195
Lee-Ha Young <98532217+Rumisister@users.noreply.github.com>
2022-04-27 10:26:24 +0900

    Modify:로그인 및 회원가입 오류 수정

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,22 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Signup from './Signup/Signup';
 import './Login.scss';
 
-const originalTitle = {
-  title: '안녕하세요',
-  paragraph: '로그인 및 회원가입을 위한 이메일 주소를 입력 부탁드립니다.',
-  inputs: [],
-  buttonContent: '계속',
-};
-
 const Login = () => {
-  const resObj = {
-    'invalid email': ['password', 'passwordCheck', 'lastName', 'firstName'],
-    SUCCESS: ['email', 'password'],
+  const originalTitle = {
+    title: '안녕하세요',
+    paragraph: '로그인 및 회원가입을 위한 이메일 주소를 입력 부탁드립니다.',
+    inputs: [],
+    buttonContent: '계속',
   };
-
   const [modalTitle, setModalTitle] = useState(originalTitle);
-
   const [userInfo, setUserInfo] = useState({
     email: '',
     password: null,
@@ -27,11 +20,15 @@ const Login = () => {
     checkBoxTwo: null,
   });
 
-  const [modalOpen, setModalOpen] = useState(false);
   const [condition, setCondition] = useState(false);
 
-  const modalClose = () => {
-    setModalOpen(!modalOpen);
+  // const modalClose = () => {
+  //   setModalOpen(!modalOpen);
+  // };
+
+  const resObj = {
+    'invalid email': ['password', 'passwordCheck', 'lastName', 'firstName'],
+    SUCCESS: ['email', 'password'],
   };
 
   const onChange = e => {
@@ -57,49 +54,49 @@ const Login = () => {
     });
   };
 
-  // useEffect(() => {}, [condition]);
-
   const onBackBtnClick = () => {
-    console.log('뒤로');
-    // ...modalTitle
     setCondition(false);
     setModalTitle(originalTitle);
   };
 
   //패치 받았을 때 N 받으면 signup으로 넘어가기
-  if ('response'.message === 'Invalid email') {
-    setModalTitle({
-      title: '푀세아에 오신 것을 환영합니다.',
-      paragraph: '회원가입을 위해 아래의 세부 정보를 작성해주세요.',
-    });
-    userInfo();
-  }
+  // if ('response'.message === 'Invalid email') {
+  //   setModalTitle({
+  //     title: '푀세아에 오신 것을 환영합니다.',
+  //     paragraph: '회원가입을 위해 아래의 세부 정보를 작성해주세요.',
+  //     buttonContent: '가입',
+  //   });
+  //   userInfo();
+  // }
 
   return (
-    <div className="modalOverlay">
-      <form className="modalBody" onSubmit={goTosignup}>
-        <button className="modalExit" type="button" onClick={modalClose}>
-          X
-        </button>
-        <div className="modalBox">
-          <div className="modalText">
-            <h2 className="modalTitle">{modalTitle.title}</h2>
-            <p className="modalParagraph">{modalTitle.paragraph}</p>
+    <>
+      <button className="loginBtn">로그인</button>
+      <div className="modalOverlay">
+        <form className="modalBody" onSubmit={goTosignup}>
+          <button className="modalExit" type="button">
+            X
+          </button>
+          <div className="modalBox">
+            <div className="modalText">
+              <h2 className="modalTitle">{modalTitle.title}</h2>
+              <p className="modalParagraph">{modalTitle.paragraph}</p>
+            </div>
+            <input
+              className="modalInput inputEmail"
+              placeholder="이메일 주소"
+              type="email"
+              name="email"
+              onChange={onChange}
+            />
+            {condition && (
+              <Signup onBackBtnClick={onBackBtnClick} onChange={onChange} />
+            )}
+            <button className="continueBtn">{modalTitle.buttonContent}</button>
           </div>
-          <input
-            className="modalInput inputEmail"
-            placeholder="이메일 주소"
-            type="email"
-            name="email"
-            onChange={onChange}
-          />
-          {condition && (
-            <Signup onBackBtnClick={onBackBtnClick} onChange={onChange} />
-          )}
-          <button className="loginBtn">{modalTitle.buttonContent}</button>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 };
 
