b7b6a7226de0fc39f0fae63563d32b6fa03118ee
Lee-Ha Young <98532217+Rumisister@users.noreply.github.com>
2022-04-27 18:09:00 +0900

    Modify:상수 자리 변경

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,84 +2,6 @@ import React, { useState } from 'react';
 import FormLayout from './FormLayout/FormLayout';
 import './Login.scss';
 
-const RESPONSE_OBJECT = {
-  initial: {
-    title: '안녕하세요',
-    paragraph: '로그인 및 회원가입을 위한 이메일 주소를 입력 부탁드립니다.',
-    inputs: [
-      {
-        type: 'email',
-        name: 'email',
-        placeholder: '이메일 주소',
-      },
-    ],
-    btnContent: '계속',
-  },
-  'invalid email': {
-    title: '푀세아에 오신 것을 환영합니다.',
-    paragraph: '회원가입을 위해 아래 세부 정보를 작성해주세요.',
-    inputs: [
-      {
-        type: 'email',
-        name: 'email',
-        placeholder: '이메일 주소',
-      },
-      {
-        type: 'password',
-        name: 'password',
-        placeholder: '패스워드',
-      },
-      {
-        type: 'password',
-        name: 'passwordCheck',
-        placeholder: '패스워드 확인',
-      },
-      {
-        type: 'text',
-        name: 'lastName',
-        placeholder: '성',
-      },
-      {
-        type: 'text',
-        name: 'firstName',
-        placeholder: '이름',
-      },
-    ],
-    btnContent: '가입',
-    checkBoxes: [
-      {
-        type: 'checkBox',
-        className: 'checkBox',
-        name: 'checkBox',
-        id: 'checkBox',
-      },
-      {
-        type: 'checkBox',
-        className: 'checkBox',
-        name: 'checkBoxTwo',
-        id: 'checkBoxTwo',
-      },
-    ],
-  },
-  SUCCESS: {
-    title: '푀세아에 다시 오신 것을 환영합니다.',
-    paragraph: '패스워드를 입력해주세요.',
-    inputs: [
-      {
-        type: 'email',
-        name: 'email',
-        placeholder: '이메일 주소',
-      },
-      {
-        type: 'password',
-        name: 'password',
-        placeholder: '패스워드',
-      },
-    ],
-    btnContent: '로그인',
-  },
-};
-
 const Login = () => {
   const [serverMessage, setServerMessage] = useState('invalid email');
   const [userInfo, setUserInfo] = useState({
@@ -176,3 +98,81 @@ const Login = () => {
 };
 
 export default Login;
+
+const RESPONSE_OBJECT = {
+  initial: {
+    title: '안녕하세요',
+    paragraph: '로그인 및 회원가입을 위한 이메일 주소를 입력 부탁드립니다.',
+    inputs: [
+      {
+        type: 'email',
+        name: 'email',
+        placeholder: '이메일 주소',
+      },
+    ],
+    btnContent: '계속',
+  },
+  'invalid email': {
+    title: '푀세아에 오신 것을 환영합니다.',
+    paragraph: '회원가입을 위해 아래 세부 정보를 작성해주세요.',
+    inputs: [
+      {
+        type: 'email',
+        name: 'email',
+        placeholder: '이메일 주소',
+      },
+      {
+        type: 'password',
+        name: 'password',
+        placeholder: '패스워드',
+      },
+      {
+        type: 'password',
+        name: 'passwordCheck',
+        placeholder: '패스워드 확인',
+      },
+      {
+        type: 'text',
+        name: 'lastName',
+        placeholder: '성',
+      },
+      {
+        type: 'text',
+        name: 'firstName',
+        placeholder: '이름',
+      },
+    ],
+    btnContent: '가입',
+    checkBoxes: [
+      {
+        type: 'checkBox',
+        className: 'checkBox',
+        name: 'checkBox',
+        id: 'checkBox',
+      },
+      {
+        type: 'checkBox',
+        className: 'checkBox',
+        name: 'checkBoxTwo',
+        id: 'checkBoxTwo',
+      },
+    ],
+  },
+  SUCCESS: {
+    title: '푀세아에 다시 오신 것을 환영합니다.',
+    paragraph: '패스워드를 입력해주세요.',
+    inputs: [
+      {
+        type: 'email',
+        name: 'email',
+        placeholder: '이메일 주소',
+      },
+      {
+        type: 'password',
+        name: 'password',
+        placeholder: '패스워드',
+      },
+    ],
+    btnContent: '로그인',
+  },
+};
