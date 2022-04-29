e56826acfeee3074981156106caa8d6cab69981b
Lee-Ha Young <98532217+Rumisister@users.noreply.github.com>
2022-04-29 22:28:10 +0900

    Add: 에러메세지 상수데이터 추가

---
+++ src/pages/Login/SIGNUP_MESSAGE.js
@@ -0,0 +1,6 @@
+export const SIGNUP_MESSAGE = {
+  samePassword: '이전에 사용했던 패스워드를 입력하세요.',
+  fullInput: '입력조건을 다 채워주세요.',
+  checkInCheckBox: '체크박스를 체크해주세요.',
+  userNameCheck: '사용자의 성, 이름을 입력해주세요.',
+};
