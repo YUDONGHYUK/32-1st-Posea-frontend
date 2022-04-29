618c9a7698be31ad1cfb120239259eb59826d397
doyu <ydh0905@nate.com>
2022-04-30 02:10:07 +0900

    Modify: MainContext 클래스 이름 수정 완료

--- src/pages/Main/MainContext/SectionContext.js
+++ src/pages/Main/MainContext/SectionContext.js
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 import './SectionContext.scss';
 
 const MainContext = () => (
-  <div className="SectionContextWrapper">
-    <span className="SectionContextHeader text1">
+  <div className="sectionContextWrapper">
+    <span className="sectionContextHeader text1">
       호기심으로 물든 일곱 가지 아로마
     </span>
-    <span className="SectionContextHeader text2">
+    <span className="sectionContextHeader text2">
       몸과 마음에 머무는 아로마
     </span>
-    <p className="SectionContextParagraph">
+    <p className="sectionContextParagraph">
       자아와 감각을 깨우는 아로마의 이솝 오 드 퍼퓸은 조향에 대한 일반적인
       관행을 따르지 않는 미묘한 차이의 접근법으로 만들어집니다.
     </p>
     <Link to="/">
-      <div className={`InnerLinkWrapper `}>
+      <div className="innerLinkWrapper">
         <span>향수 보기</span>
         <i className="fa-solid fa-arrow-right" />
       </div>
